7024de6ab9e977cf1809099b0bc63086b97182d0
Marco Zimmermann <fuzzyslash@hotmail.com>
2026-01-22 12:24:56 -0600

    Leading highlight

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@ import ScoreCol from "./ScoreCol.js";
 const scoreColHome = document.getElementById('home');
 const scoreColGuest = document.getElementById ('guest');
 
-new ScoreCol(scoreColHome);
-new ScoreCol(scoreColGuest);
+const home = new ScoreCol(scoreColHome);
+const guest = new ScoreCol(scoreColGuest);
+
+home.scoreChanged.push(onScoreChanged);
+guest.scoreChanged.push(onScoreChanged);
+
+function onScoreChanged ()
+{
+    home.el.classList.remove('score-col--leading')
+    guest.el.classList.remove('score-col--leading')
+
+    if(home.score > guest.score)
+    {
+        home.el.classList.add('score-col--leading')
+    }
+    else if (guest.score > home.score)
+    {
+        guest.el.classList.add('score-col--leading')
+    }
+
+
+}
